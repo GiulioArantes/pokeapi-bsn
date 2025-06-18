5741a465a94cc0742e45cdfe8551d7fb67be5287
GiulioArantes <giulio.arantes@icloud.com>
2025-06-17 22:30:24 -0300

    feat: tela home simples, com adicional de 3 informações sobre o pokemon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { NgIf, NgFor, TitleCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [NgFor, NgIf, FormsModule, TitleCasePipe, IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  pokemonName = '';
+  pokemon: any = null;
+  loading = false;
+  error = '';
+
+  constructor(private pokemonService: PokemonService) {}
+
+  loadPokemon() {
+    if (!this.pokemonName) {
+      this.pokemon = null;
+      this.error = 'Digite o nome de um Pokémon!';
+      return;
+    }
+    this.loading = true;
+    this.error = '';
+    this.pokemon = null;
+
+    this.pokemonService.getPokemon(this.pokemonName.toLowerCase()).subscribe({
+      next: (data) => {
+        this.pokemon = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Pokémon não encontrado!';
+        this.loading = false;
+      },
+    });
+  }
 }
